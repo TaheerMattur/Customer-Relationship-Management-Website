feb86b2f866dde7626b65c643c85173512a4d8bb
Taheer Mattur <md.taheermattur@gmail.com>
2021-09-07 13:33:56 +0530

    CRM Website

---
+++ script.js
@@ -0,0 +1,34 @@
+$("#search-icon").click(function () {
+  $(".nav").toggleClass("search");
+  $(".nav").toggleClass("no-search");
+  $(".search-input").toggleClass("search-active");
+  });
+
+  $(".menu-toggle").click(function () {
+  $(".nav").toggleClass("mobile-nav");
+  $(this).toggleClass("is-active");
+  });
+
+// const searchIcon = document.getElementById("search-icon");
+// const nav = document.getElementsByClassName("nav");
+// const searchInput = document.getElementsByClassName("search-input");
+// const menuToggle = document.getElementById("menu-toggle");
+
+// searchIcon.addEventListener("click", () => {
+//   console.log("yess");
+//   nav.classList.toggle("search");
+//   nav.classList.toggle("no-search");
+//   searchInput.classList.toggle("search-active");
+// });
+
+// menuToggle.addEventListener("click", () =>  {
+//   console.log("yess");
+//   nav.classList.toggle("mobile-nav");
+//   this.classList.toggle("is-active");
+// });
+
+// IFRAME RESIZER
+var iframe = document.getElementById("myIframe");
+iframe.onload = function() {
+    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+}
