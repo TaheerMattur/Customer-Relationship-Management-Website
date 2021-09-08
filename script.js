6897062e658104a66690cfa3df29c02b634a52d6
Taheer Mattur <md.taheermattur@gmail.com>
2021-09-09 05:06:59 +0530

    resolved UI Bugs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* // JQUERY CODE
 $("#search-icon").click(function () {
   $(".nav").toggleClass("search");
   $(".nav").toggleClass("no-search");
@@ -8,27 +9,24 @@ $("#search-icon").click(function () {
   $(".nav").toggleClass("mobile-nav");
   $(this).toggleClass("is-active");
   });
+*/
 
-// const searchIcon = document.getElementById("search-icon");
-// const nav = document.getElementsByClassName("nav");
-// const searchInput = document.getElementsByClassName("search-input");
-// const menuToggle = document.getElementById("menu-toggle");
+const searchIcon = document.getElementById("search-icon");
+const nav = document.getElementsByClassName("nav");
+const searchInput = document.getElementsByClassName("search-input");
+const menuToggle = document.getElementById("mobile-menu");
 
-// searchIcon.addEventListener("click", () => {
-//   console.log("yess");
-//   nav.classList.toggle("search");
-//   nav.classList.toggle("no-search");
-//   searchInput.classList.toggle("search-active");
-// });
+searchIcon.addEventListener("click", () => {
+    // console.log(nav);
+  nav[0].classList.toggle("search");
+  nav[0].classList.toggle("no-search");
+  // console.log(searchInput);
+  searchInput[0].classList.toggle("search-active");
+});
 
-// menuToggle.addEventListener("click", () =>  {
-//   console.log("yess");
-//   nav.classList.toggle("mobile-nav");
-//   this.classList.toggle("is-active");
-// });
-
-// IFRAME RESIZER
-// var iframe = document.getElementById("myIframe");
-// iframe.onload = function() {
-//     iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-// }
+menuToggle.addEventListener("click", (event) =>  {
+      // console.log(nav);
+      nav[0].classList.toggle("mobile-nav");
+      // console.log(event.currentTarget);
+      event.currentTarget.classList.toggle("is-active");
+});
